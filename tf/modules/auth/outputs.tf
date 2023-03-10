18e0e4dcf53f7cbaacc8128f2638f26e361c035b
Vladimir Dabic <vladadaba92@gmail.com>
2023-03-10 19:19:53 +0100

    change auth outputs

--- tf/modules/auth/outputs.tf
+++ tf/modules/auth/outputs.tf
@@ -1,5 +1,10 @@
+locals {
+  cognito_user_pool_id = module.cognito.cognito_user_pool_id
+  cognito_issuer_uri   = "https://cognito-idp.${var.region}.amazonaws.com/${local.cognito_user_pool_id}"
+}
+
 output "cognito_user_pool_id" {
-  value = module.cognito.cognito_user_pool_id
+  value = local.cognito_user_pool_id
 }
 
 output "cognito_client_id" {
@@ -12,11 +17,11 @@ output "cognito_client_secret" {
 }
 
 output "cognito_issuer_uri" {
-  value = "auth.${var.domain_name}" # "https://cognito-idp.${var.region}.amazonaws.com/${aws_cognito_user_pool.pool.id}"
+  value = local.cognito_issuer_uri # "https://cognito-idp.${var.region}.amazonaws.com/${aws_cognito_user_pool.pool.id}"
 }
 
 output "cognito_jwk_uri" {
-  value = "auth.${var.domain_name}/.well-known/jwks.json" # https://aws.amazon.com/premiumsupport/knowledge-center/decode-verify-cognito-json-token/
+  value = "${local.cognito_issuer_uri}/.well-known/jwks.json" # https://aws.amazon.com/premiumsupport/knowledge-center/decode-verify-cognito-json-token/
 }
 
 output "cognito_confirm_user_base_url" {
